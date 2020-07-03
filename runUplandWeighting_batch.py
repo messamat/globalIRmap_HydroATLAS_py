d2b6519225fb69ba98b0c2e6e4c081a2d657a1ee
messamat <messamat@uw.edu>
2020-07-03 02:21:32 -0400

    Correct glitch in CMI flow acc, add SoilGrids in runUplandWeighting_batch.py

--- runUplandWeighting_batch.py
+++ runUplandWeighting_batch.py
@@ -12,10 +12,8 @@ directionras = os.path.join(geomdir, 'Flow_directions', 'flow_dir_15s_global.gdb
 weightras = os.path.join(geomdir,'Accu_area_grids', 'pixel_area_skm_15s.gdb', 'px_area_skm_15s')
 uplandras = os.path.join(geomdir, 'Accu_area_grids', 'upstream_area_skm_15s.gdb', 'up_area_skm_15s')
 
-#Value grids
-worldclim_value = getfilelist(os.path.join(resdir, 'worldclimv2.gdb'), '.*nibble$')
-
-def hydroUplandWeighting(value_grid, direction_grid, weight_grid, upland_grid, scratch_dir, out_dir, overwrite=False):
+def hydroUplandWeighting(value_grid, direction_grid, weight_grid, upland_grid, scratch_dir,
+                        out_dir, shiftval=0, overwrite=False):
     #Check that directories exist, otherwise, create them
     pathcheckcreate(scratch_dir)
     pathcheckcreate(out_dir)
@@ -34,12 +32,12 @@ def hydroUplandWeighting(value_grid, direction_grid, weight_grid, upland_grid, s
 
             # Multiply input grid by pixel area
             print('Processing {}...'.format(xpxarea))
-            valueXarea = Times(r"" + value_grid, r"" + weight_grid)
+            valueXarea = Times(Raster(value_grid) + float(shiftval), Raster(weight_grid))
             valueXarea.save(xpxarea)
 
             # Flow accumulation of value grid and pixel area product
             print('Processing {}...'.format(xpxarea_ac1))
-            outFlowAccumulation = FlowAccumulation(direction_grid, xpxarea, "FLOAT")
+            outFlowAccumulation = FlowAccumulation(direction_grid, Raster(xpxarea), "FLOAT")
             outFlowAccumulation.save(xpxarea_ac1)
 
             print('Processing {}...'.format(xpxarea_ac_fin))
@@ -65,6 +63,11 @@ def hydroUplandWeighting(value_grid, direction_grid, weight_grid, upland_grid, s
     else:
         print('{} already exists and overwrite==False...'.format(out_grid))
 
+
+############################ Worlclim v2 ###############################################################################
+#Value grids
+worldclim_value = getfilelist(os.path.join(resdir, 'worldclimv2.gdb'), '.*nibble$')
+
 for wc_valuegrid in worldclim_value:
     hydroUplandWeighting(value_grid = wc_valuegrid,
                          direction_grid = directionras,
@@ -72,3 +75,34 @@ for wc_valuegrid in worldclim_value:
                          upland_grid = uplandras,
                          scratch_dir = os.path.join(resdir, 'scratch.gdb'),
                          out_dir = os.path.join(resdir, 'worldclimv2.gdb'))
+
+
+############################ Global Aridity Index and CMI v2 ###########################################################
+et0_outdir = os.path.join(datdir, 'GAIv2')
+et0resgdb = os.path.join(resdir, 'et0.gdb')
+et0var = {os.path.splitext(os.path.split(lyr)[1])[0]:lyr for lyr in getfilelist(et0_outdir,'(ai_)*et0(_[0-9]{2})*[.]tif$')}
+cmidict = {"cmi_{}".format(str(mnth).zfill(2)): os.path.join(et0resgdb, 'cmi_{}'.format(str(mnth).zfill(2))) for mnth in xrange(1,13)}
+et0var.update(cmidict)
+et0nib = {var:os.path.join(et0resgdb, '{}_nibble2'.format(var)) for var in et0var}
+
+for etcmi_valuegrid in cmidict:
+    hydroUplandWeighting(value_grid = etcmi_valuegrid,
+                         direction_grid = directionras,
+                         weight_grid = weightras,
+                         upland_grid = uplandras,
+                         scratch_dir = os.path.join(resdir, 'scratch.gdb'),
+                         out_dir = et0resgdb,
+                         shiftval = 100)
+
+
+############################ SoilGrids250m v2 ##########################################################################
+sgresgdb = os.path.join(resdir, 'soilgrids250.gdb')
+sgdict = getfilelist(sgresgdb, '.*_wmeanagg2$')
+
+for sg_valuegrid in sgdict:
+    hydroUplandWeighting(value_grid = sg_valuegrid,
+                         direction_grid = directionras,
+                         weight_grid = weightras,
+                         upland_grid = uplandras,
+                         scratch_dir = os.path.join(resdir, 'scratch.gdb'),
+                         out_dir = sgresgdb)
