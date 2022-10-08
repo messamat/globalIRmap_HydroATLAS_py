4d7a80ef4dd2bf6a48c4092b1ac4af22d017826b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-08 02:39:01 +0000

    Adding tarfile member sanitization to extractall()

--- archived/download_earthenvdem90.py
+++ archived/download_earthenvdem90.py
@@ -34,4 +34,26 @@ for f in ee_tarlist:
     print(f)
     if not os.path.exists('{}.bil'.format(os.path.splitext(f)[0])):
         with tarfile.open(f) as ftar:
-            ftar.extractall(ee_outdir)  # specify which folder to extract to
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(ftar, ee_outdir)
